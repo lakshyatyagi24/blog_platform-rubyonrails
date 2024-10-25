32fd44fa49059fc34f08ace4a33ca9a060c94ac0
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2024-10-26 01:05:32 +0530

    changed key

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the Rails application.
 require_relative "application"
 
-# Ensure the secret is set correctly
-@secret = ENV['SECRET_KEY_BASE'] || SecureRandom.hex(16) # Generates a 32-character hex string
+# Ensure the secret is set correctly, use 32 characters (16 bytes will not be sufficient for aes-256-cbc)
+@secret = ENV['SECRET_KEY_BASE'] || SecureRandom.hex(32) # Generates a 32-byte hex string (64 characters)
 
-# If @secret is not 32 bytes long, ensure it is
-@secret = @secret[0, 32] if @secret.length > 32
+# Ensure that @secret is exactly 32 bytes for AES-256
+@secret = @secret[0, 32]
 
 # Initialize the Rails application.
 Rails.application.initialize!
