c8647cc990e45f43b2608d95b6d4aba6c6f3f1f7
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2024-10-29 14:08:10 +0530

    Added Devise authentication and fixed UI issue with repeated post success message.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :posts do
     resources :comments, only: [:create]
   end
@@ -8,6 +9,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
   root to: "posts#index"
 end
